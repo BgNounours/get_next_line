88479968cdfa109c96dc7bc3d521597caea67a98
Joris Schaft <jschaft@c3r4p6.42perpignan.fr>
2023-12-18 14:29:47 +0100

    [ADD] everything

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschaft <cecile.schaft@orange.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 10:55:21 by jschaft           #+#    #+#             */
+/*   Updated: 2023/11/03 12:34:47 by jschaft          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin_get(char *s, char *buff);
+char	*ft_strchr_get(char *s, int c);
+
+size_t	ft_strlen_get(char const *s);
+
+#endif
